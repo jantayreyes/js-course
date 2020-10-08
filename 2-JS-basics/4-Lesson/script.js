0be4a421c2bf2db333f3a4c51be399955d3016e6
jantayreyes <jantayreyes@gmail.com>
2020-10-08 19:25:35 +0200

    Operators

--- 2-JS-basics/4-Lesson/script.js
+++ 2-JS-basics/4-Lesson/script.js
@@ -1,4 +1,4 @@
-
+/*
 //Basic Operators
 
 var yearNow, yearJohn, yearMark;
@@ -25,6 +25,34 @@ console.log(typeof "I do not where I live 10 years ago");
 var x;
 console.log(typeof x);
 
-
+*/
 // Operator precedence
 
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18
+ // Muliple operators 
+var isFulllAge =  now - yearJohn >= fullAge; // true
+console.log(isFulllAge);
+
+// Groupin
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assigments
+
+var x, y;
+
+x = y = (3 + 5) * 4 - 6; // 8*4 - 6// 32-6// 26
+console.log(x, y);
+
+// More operators
+
+x = x * 2;
+y *= 2;
+console.log(x, y); // 52, 52
+
+x++
+console.log(x);
