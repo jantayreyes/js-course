e161f21cca5d4cf404d076b0d3c28da7dc70614c
jantayreyes <jantayreyes@gmail.com>
2020-10-08 18:56:51 +0200

    fafa2

--- 2-JS-basics/3-Lesson/script.js
+++ 2-JS-basics/3-Lesson/script.js
@@ -48,30 +48,3 @@ var lastName = prompt('What is her last name?');
 console.log(firstName + ' ' + lastName);
 */
 
-//Basic Operators
-
-var yearNow, yearJohn, yearMark;
-ageJohn = 28;
-ageMark =  33;
-
-// Math Operators
-yearNow = 2020;
-yearJohn  = yearNow - ageJohn;
-yearMark =  yearNow - ageMark;
-console.log(yearJohn);
-console.log(yearMark);
-console.log(yearNow + 5);
-console.log(yearNow / 10);
-
-// Logical Operators
-var johnOlder = ageJohn > ageMark;
-console.log(johnOlder);
-
-// typeof 0perator => say to us the type of variable we got.
-console.log(typeof johnOlder);
-console.log(typeof ageJohn);
-console.log(typeof "I do not where I live 10 years ago");
-var x;
-console.log(typeof x);
-
-
