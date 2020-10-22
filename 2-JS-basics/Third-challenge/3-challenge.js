7655ef42d3f97553bafeb0b3652320f6f04fa7af
jantayreyes <jantayreyes@gmail.com>
2020-10-22 20:42:19 +0200

    Third Challenge done

---
+++ 2-JS-basics/Third-challenge/3-challenge.js
@@ -0,0 +1,44 @@
+'use strict'
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+5. Ignore draws this time
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+*/
+
+
+  
+const calAverage = (x, y, z) => (x + y + z) / 3;
+
+const scoreDolphins = calAverage(85, 54, 41);
+const scoreKoalas = calAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+
+function checkWiner (averageDolphins, averageKoalas) {
+  if (averageDolphins >= 2 * averageKoalas) {
+    console.log(`Dolphins won the game (${averageDolphins} vs. ${averageKoalas})`);
+  } else if (averageKoalas >= 2* averageDolphins) {
+    console.log(`Koalas won the game (${averageKoalas} vs. ${averageDolphins})`);
+  } else {
+    console.log('Neither team won the game');
+  }
+}
+console.log(checkWiner(scoreDolphins, scoreKoalas));
+
