4543faca8decd50816d01ce46ef3fcab3722ad96
jantayreyes <jantayreyes@gmail.com>
2020-10-27 15:10:31 +0100

    While and for Loops

--- 2-JS-basics/16-Lesson/script.js
+++ 2-JS-basics/16-Lesson/script.js
@@ -103,6 +103,7 @@ while (reps <= 10) {
 
 let dice = Math.trunc(Math.random() * 6) + 1;
 while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
   dice = Math.trunc(Math.random() * 6) + 1;
   if (dice === 6) {
     console.log('Loop is about to end...');
