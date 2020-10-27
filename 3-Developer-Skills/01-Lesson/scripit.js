6038aaa8eb7c9afaa34f17a17ea54c04480f06f9
jantayreyes <jantayreyes@gmail.com>
2020-10-27 21:57:55 +0100

    Not sense

--- 3-Developer-Skills/01-Lesson/scripit.js
+++
@@ -1,6 +0,0 @@
-
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
-
-const x = 23;
-
---
+++ 3-Developer-Skills/01-Lesson/script.js
@@ -0,0 +1,11 @@
+
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = 23;
+
+const calcAge = birthYear => 2020 - birthYear
+
+console.log(calcAge(1995));
+console.log(2 + 5);
+console.log(2 + 155);
