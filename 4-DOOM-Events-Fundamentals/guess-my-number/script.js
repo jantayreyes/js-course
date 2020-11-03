ab52549358547d18bda56b3c34506167dbf307a7
jantayreyes <jantayreyes@gmail.com>
2020-11-04 00:16:09 +0100

    Added some css style

--- 4-DOOM-Events-Fundamentals/guess-my-number/script.js
+++ 4-DOOM-Events-Fundamentals/guess-my-number/script.js
@@ -19,10 +19,21 @@ document.querySelector('.check').addEventListener('click', function(){
   // We must compare the random number with the guess number, for that we need that both  have to be a number variable.
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+  // If there is not a valid input
   if (!guess) {
     document.querySelector('.message').textContent = 'Please, write a number 😒';
+  // If player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '😜 Correct Number!!';
+    document.querySelector('body').style.backgroundColor = '#80b347';
+    document.querySelector('.number').style.borderRadius = '50px';
+    document.querySelector('.number').style.backgroundColor = '#FCF3CF';
+    document.querySelector('.number').style.width = '30rem';
+
+
+
+    // CSS = background-color === backgroundColor = JS * works for all the CSS properties
+  // input higher than secret number
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '😡 Too high!';
@@ -32,6 +43,7 @@ document.querySelector('.check').addEventListener('click', function(){
       document.querySelector('.message').textContent = 'You have lost! 👺';
       document.querySelector('.score').textContent = 0;
     }
+    // input lower than secret number
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '🤢 Too low!';
