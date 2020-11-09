b313bc2f4639a8eec1d1315d23d45a618562d880
jantayreyes <jantayreyes@gmail.com>
2020-11-09 20:03:58 +0100

    Added starting conditions

--- 4-DOOM-Events-Fundamentals/pig-game/script.js
+++ 4-DOOM-Events-Fundamentals/pig-game/script.js
@@ -1,2 +1,16 @@
-console.log('Testing');
-console.log('Testing x2');
+'use strict';
+
+//Selecting elements 
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+console.log(score0El);
+console.log(score1El);
+console.log(diceEl);
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
