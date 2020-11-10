77dd4f6fc547464d051edeed58001fe87f28cd44
jantayreyes <jantayreyes@gmail.com>
2020-11-10 15:49:12 +0100

    Game finished

--- 4-DOOM-Events-Fundamentals/pig-game/script.js
+++ 4-DOOM-Events-Fundamentals/pig-game/script.js
@@ -12,16 +12,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-
+let scores, currentScore, activePlayer, playing;
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+ 
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing =  true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing =  true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+// to start the game
+init();
 
 const switchPlayer =  function() {
   document.getElementById(`current--${activePlayer}`).textContent = 0
@@ -36,7 +48,7 @@ btnRoll.addEventListener('click', function() {
   if (playing) {
     // 1 - Generatin a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    
+
     // 2 - Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`
@@ -62,7 +74,7 @@ btnHold.addEventListener('click', function() {
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
     // 2 - Check if player's score is >= 100
-    if(scores[activePlayer] >= 20) {
+    if(scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -74,3 +86,7 @@ btnHold.addEventListener('click', function() {
     }
   }
 });
+
+btnNew.addEventListener('click', function () {
+  init();
+})
