c88df5cd0e0b1cae965a937509a57ae42e4a2750
jantayreyes <jantayreyes@gmail.com>
2020-11-13 01:32:32 +0100

    Destructuring Objects

--- 6-Data-Structures-Operators/02-Lesson/script.js
+++ 6-Data-Structures-Operators/02-Lesson/script.js
@@ -21,4 +21,58 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return (this.starterMenu[starterIndex], this.mainMenu[mainIndex]);
+  },
+
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  }
 };
+
+restaurant.orderDelivery({
+  time: '23:19',
+  address: 'Via del carrer 25',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Calle Loreno 25',
+  time: '15:40',
+})
+
+// Destructuring Objects
+
+const  { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// Changing the property name
+const { 
+  name: restaurantName, 
+  openingHours: hours, 
+  categories: tags 
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = ['Chicken', 'Fries'], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables while destructuting objects
+let a = 111;
+let b = 999;
+
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj) // You must wrap everithing into parenthesis
+console.log(a, b);
+
+// Nested Objects
+const { fri: {open: o, close: c} } = openingHours;
+console.log(o, c);  
+
+const { sat: {open, close} } = openingHours;
+console.log(open, close);  
+
