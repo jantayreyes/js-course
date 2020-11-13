463c97479709a77485c4b326efacc071e40634f8
jantayreyes <jantayreyes@gmail.com>
2020-11-13 15:06:18 +0100

    Pending

--- 6-Data-Structures-Operators/1-Challenge/01-challenge.js
+++ 6-Data-Structures-Operators/1-Challenge/01-challenge.js
@@ -29,4 +29,47 @@ Then, call the function again with players from game.scored
 GOOD LUCK �
  */
 
- 
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5,
+    },
+  };
+
+  // 1)
+  const {players: player1} = game
+  console.log(player1);
