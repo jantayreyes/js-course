d4f023bebaca274736676ecc51d4a0fcbad3adc4
jantayreyes <jantayreyes@gmail.com>
2020-11-13 16:07:47 +0100

    Challenge completed

--- 6-Data-Structures-Operators/1-Challenge/01-challenge.js
+++ 6-Data-Structures-Operators/1-Challenge/01-challenge.js
@@ -70,6 +70,42 @@ const game = {
     },
   };
 
-  // 1)
-  const {players: player1} = game
-  console.log(player1);
+// 1)
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+// 2)
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3) 
+const allPlayers = [...players1, ...players2]
+console.log(allPlayers);
+
+// 4)
+const finalPlayers = [...allPlayers, 'Thiago', 'Coutinho', 'Perisic']
+console.log(finalPlayers);
+
+// 5)
+const { team1: team1, x: draw, team2: team2 } = game.odds;
+console.log(team1, draw, team2 );
+
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
+
+// 6)
+const printGoals = function(...names) {
+  console.log(names);
+  console.log(`${names.length} goals were scored`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+// 7)
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
