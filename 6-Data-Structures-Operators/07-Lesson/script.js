039777e7e451d02a9e451356bec9126923733370
jantayreyes <jantayreyes@gmail.com>
2020-11-14 19:09:51 +0100

    Enhanced object literals

--- 6-Data-Structures-Operators/07-Lesson/script.js
+++ 6-Data-Structures-Operators/07-Lesson/script.js
@@ -1,40 +1,45 @@
 'use strict';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  // You can use any expression 
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order: function (starterIndex, mainIndex) {
+  // ES6 now you can delete : and function, it will work in the same way that before.
+  order(starterIndex, mainIndex) {
     return (this.starterMenu[starterIndex], this.mainMenu[mainIndex]);
   },
+  // ES6 ENHANCED OBJECT LITERALS
+  openingHours,
 
-  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
     console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients); 
   },
@@ -54,3 +59,6 @@ for(const [i, el] of menu.entries()){
   console.log(`${i + 1}: ${el}`);
 };
 // console.log(menu.entries()); - Array iterator
+
+//ENHANCED OBJECT LITERALS
+
