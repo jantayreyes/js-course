b3da600ba4a812de754977d626b675e58ce4ed19
jantayreyes <jantayreyes@gmail.com>
2020-11-14 18:55:44 +0100

    for-of loop

--- 6-Data-Structures-Operators/07-Lesson/script.js
+++ 6-Data-Structures-Operators/07-Lesson/script.js
@@ -40,4 +40,17 @@ const restaurant = {
   },
 };
 
-console.log('afa');
+// FOR-OF LOOP
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for(const item of menu) console.log(item);
+console.log('---- OLD SCHOOL ----');
+for(const item of menu.entries()){
+  console.log(`${item[0] + 1}: ${item[1]}`);
+};
+
+console.log('---- COOL SCHOOL ----');
+for(const [i, el] of menu.entries()){
+  console.log(`${i + 1}: ${el}`);
+};
+// console.log(menu.entries()); - Array iterator
