7a61cde868f1a777c1b328d03d1fc75d747e2525
jantayreyes <jantayreyes@gmail.com>
2020-11-15 18:19:41 +0100

    Looping Objects: Object KEY, VALUE AND ENTRIES

--- 6-Data-Structures-Operators/09-Lesson/script.js
+++ 6-Data-Structures-Operators/09-Lesson/script.js
@@ -44,4 +44,27 @@ const restaurant = {
   },
 };
 
-console.log('fafa');
+// Property NAMES/KEYS
+const properties =  Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days`
+
+for (const day of properties) {
+  openStr += `${day}, `
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// PROPERTY KEYS AND VALUES - Entire object
+const entries = Object.entries(openingHours)
+// console.log(entries);
+
+// The key is the first one element of the entries, and the value is the second one |We destructuring the value into the two properties name that we need. you can see in the console.log('entries);
+//           Key -  Value
+for (const [key, {open, close}] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
