19fb4aaa8a2e0e21896587fe3b53bed814b20370
jantayreyes <jantayreyes@gmail.com>
2020-11-23 15:26:03 +0100

    Functions returning functions

--- 7-Advanced-Functions/04-Lesson/script.js
+++ 7-Advanced-Functions/04-Lesson/script.js
@@ -1,3 +1,22 @@
 'use strict';
 
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  }
+};
 
+const greeterHey = greet('Hey');
+greeterHey('Jesus');
+greeterHey('Steven');
+
+greet('Hello')('Jesus');
+
+// Arrow functions
+const waves = (greeting ) => (name) => console.log(`${greeting} ${name}`);
+
+waves('Hallo')('Jesus');
+
+const greeterWave = waves('Nice to see you');
+greeterWave('Laura');
+greeterWave('Robert');
