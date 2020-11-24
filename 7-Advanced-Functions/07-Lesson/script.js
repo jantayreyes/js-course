24804f50ed55abed2fa4676d4c29983a40018cba
jantayreyes <jantayreyes@gmail.com>
2020-11-24 13:52:45 +0100

    Closure examples

--- 7-Advanced-Functions/07-Lesson/script.js
+++ 7-Advanced-Functions/07-Lesson/script.js
@@ -41,4 +41,46 @@ booker();
 booker();
 
 console.dir(booker);
+
 booker();
+
+// Example 1
+let f;
+const g =  function() {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  }
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  }
+};
+
+g();
+f();
+console.dir(f);
+// Re-assigning function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function(n, wait) {
+  const perGroup = n / 3;
+  
+  setTimeout(function(){
+    console.log(`Will are now boarding all ${n} passangers`);
+    console.log(`There are 3 groups, each with ${perGroup} passangers`);
+  }, wait * 1000) // two arguments, first a function to be executed and the second the time in miliseconds.
+
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// The closure even has priority over the scope chain.
+const perGroup = 1000;
+boardPassengers(180, 3);
