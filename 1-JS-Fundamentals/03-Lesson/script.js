d609e9e01c0beb2a0d364230d8599c8f7f430a0d
jantayreyes <jantayreyes@gmail.com>
2020-11-24 14:23:57 +0100

    Declaring variables VAR

---
+++ 1-JS-Fundamentals/03-Lesson/script.js
@@ -0,0 +1,43 @@
+
+ // Variables and data types
+
+
+console.log(firstName);
+console.log('Your name is: ' + firstName);
+
+var lastName = 'Smith';
+var age =  28;
+var fullAge = true;
+
+console.log(lastName);
+
+var job; 
+console.log(job);
+job = 'Teacher';
+console.log(job);
+
+
+// Type coercions
+var firstName = 'Fanny';
+var age = 41;
+
+console.log(firstName + ' ' + age)
+
+var job, isMarried;
+
+job = 'teacher',
+isMarried = false;
+
+console.log(firstName + ' has ' + age + ' years old and she is a ' + job + 
+'. Is he married? ' + isMarried);
+
+// Variable mutations
+age = 'forty five'
+
+alert(firstName + ' has ' + age + ' years old and she is a ' + job + 
+'. Is he married? ' + isMarried);
+
+var lastName = prompt('What is her last name?');
+console.log(firstName + ' ' + lastName);
+
+
