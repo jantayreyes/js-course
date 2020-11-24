e2e0a34303c88e6e062d267dfb8bed26e05571fe
jantayreyes <jantayreyes@gmail.com>
2020-11-24 17:31:35 +0100

    Started files

---
+++ 8-Arrays/02-Lesson/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
