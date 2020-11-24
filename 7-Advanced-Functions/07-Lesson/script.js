b32f055715a0f043a11c8b04ef127fe3fad2cd61
jantayreyes <jantayreyes@gmail.com>
2020-11-24 13:08:02 +0100

    Immediately Invoked Function Expression

--- 7-Advanced-Functions/07-Lesson/script.js
+++ 7-Advanced-Functions/07-Lesson/script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+// Sometines we need run functions just one time. After that, we need to delete/disable.
+const runOnce = function() {
+  console.log("This will never run again");
+};
+runOnce();
+
+
+// Wrapping a function into parenthesis, it will be said that is a proper function to JS. For that reason we can run the function
+(function () {
+  console.log("This will never run again");
+})();
+//Puting parenthesis at the end of the function will call it for once.
+
+(() => console.log("Also never run again"))();
+
+{
+  const isPrivate = 23;
+  var noPrivate = 40;
+}
+
+// console.log(isPrivate);
+console.log(noPrivate);
