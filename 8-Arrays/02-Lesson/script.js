9702305bbaed7e04899da41d49503feefff63773
jantayreyes <jantayreyes@gmail.com>
2020-11-24 17:48:43 +0100

    forEach loop

--- 8-Arrays/02-Lesson/script.js
+++ 8-Arrays/02-Lesson/script.js
@@ -12,3 +12,24 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// FOREACH
+console.log('----- FOR -----');
+// for(const movement of movements) {
+for(const [i, movement] of movements.entries()) {
+  if(movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited: ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+};
+
+console.log('----- FOREACH -----');
+movements.forEach(function (mov, i, arr) {
+  // console.log(arr);
+  if(mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited: ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
