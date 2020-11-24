0ac8d35c48c10dab624567ed0ffe6910d2503ab8
jantayreyes <jantayreyes@gmail.com>
2020-11-24 14:22:54 +0100

    Old school: Var Variable

---
+++ 1-JS-Fundamentals/02-Lesson/script.js
@@ -0,0 +1,17 @@
+var firstName = 'John';
+
+console.log(firstName);
+console.log('Your name is: ' + firstName);
+
+var lastName = 'Smith';
+var age =  28;
+var fullAge = true;
+
+console.log(lastName);
+
+var job; 
+
+console.log(job);
+job = 'Teacher';
+
+console.log(job);
