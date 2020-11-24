74931a4b3309a7abfbaae5fd25ad81b9b2b50a03
jantayreyes <jantayreyes@gmail.com>
2020-11-24 17:59:49 +0100

    forEach: Arrays, Maps, Sets

--- 8-Arrays/02-Lesson/script.js
+++ 8-Arrays/02-Lesson/script.js
@@ -1,19 +1,10 @@
 'use strict';
 
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
 
 // FOREACH
+// Arrays
 console.log('----- FOR -----');
 // for(const movement of movements) {
 for(const [i, movement] of movements.entries()) {
@@ -33,3 +24,26 @@ movements.forEach(function (mov, i, arr) {
     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
   }
 });
+
+// Maps
+console.log('----- FOR EACH MAPS -----');
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function(value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Sets
+console.log('----- FOR EACH SETS -----');
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+
+// The argument key does not make sense, but we let it because it is easier to understand. 
+currenciesUnique.forEach(function(value, _, map) {
+  console.log(`${_}: ${value}`);
+});
+// _ it is a convention to say a variable is completely unnecesaary.
