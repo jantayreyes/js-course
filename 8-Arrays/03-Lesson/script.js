969db0359e56093465a2e08ac880908c12278949
jantayreyes <jantayreyes@gmail.com>
2020-11-25 14:17:43 +0100

    Map

--- 8-Arrays/03-Lesson/script.js
+++ 8-Arrays/03-Lesson/script.js
@@ -1,49 +1,29 @@
 'use strict';
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// MAP
+const movements = [200, 450, -400, 3000, -650, -130, 70, -1300, 255, 212, -121212];
+const eurToUsd = 1.1;
 
+const movementsUSD = movements.map(function(mov) {
+  return mov * eurToUsd;
+});
 
-// FOREACH
-// Arrays
-console.log('----- FOR -----');
-// for(const movement of movements) {
-for(const [i, movement] of movements.entries()) {
-  if(movement > 0) {
-    console.log(`Movement ${i + 1}: You deposited: ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
-  }
+console.log(movements);
+console.log(movementsUSD);
+
+// for
+const movementsUSDfor = []
+for(const mov of movements) {
+  movementsUSDfor.push(mov * eurToUsd)
 };
+console.log(movementsUSDfor);
 
-console.log('----- FOREACH -----');
-movements.forEach(function (mov, i, arr) {
-  // console.log(arr);
-  if(mov > 0) {
-    console.log(`Movement ${i + 1}: You deposited: ${mov}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
-  }
-});
+// arrow - map
+const movementUSDarrow = movements.map(mov => mov * eurToUsd);
+console.log(movementUSDarrow);
 
-// Maps
-console.log('----- FOR EACH MAPS -----');
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-currencies.forEach(function(value, key, map) {
-  console.log(`${key}: ${value}`);
-});
-
-// Sets
-console.log('----- FOR EACH SETS -----');
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
-console.log(currenciesUnique);
-
-// The argument key does not make sense, but we let it because it is easier to understand. 
-currenciesUnique.forEach(function(value, _, map) {
-  console.log(`${_}: ${value}`);
-});
-// _ it is a convention to say a variable is completely unnecesaary.
+const movementsDescriptions = movements.map((mov, i) => 
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} 
+  ${Math.abs(mov)}`
+);
+console.log(movementsDescriptions);
