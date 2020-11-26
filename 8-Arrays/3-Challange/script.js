3f53ed3cfb7b838a58bbd36de4c80379e25bf796
jantayreyes <jantayreyes@gmail.com>
2020-11-26 15:25:20 +0100

    Challenge completed

--- 8-Arrays/3-Challange/script.js
+++ 8-Arrays/3-Challange/script.js
@@ -7,5 +7,15 @@ Test data:
 § Data 2: [16, 6, 10, 5, 6, 1, 4]
  */
 
+const calcAverageHumanAge = dogAgesArr => {
+  const averageAge = dogAgesArr
+    .map(age => age <= 2 ? age * 2 : 16 + (age * 4))
+    .filter(age => age > 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  return averageAge;
+};
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
+console.log(avg1, avg2);
 
