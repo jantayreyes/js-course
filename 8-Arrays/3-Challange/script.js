f1349c8fd3cb836a274c10c346f04ebf1b3c26eb
jantayreyes <jantayreyes@gmail.com>
2020-11-26 15:12:14 +0100

    Started files

---
+++ 8-Arrays/3-Challange/script.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+as an arrow function, and using chaining!
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+ */
+
+
+
