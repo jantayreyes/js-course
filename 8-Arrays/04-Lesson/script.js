ceed8f0d5b44e7e7c7aa030c98ba3a8bc99ba5b4
jantayreyes <jantayreyes@gmail.com>
2020-11-26 12:10:37 +0100

    Filter

--- 8-Arrays/04-Lesson/script.js
+++ 8-Arrays/04-Lesson/script.js
@@ -3,4 +3,20 @@
 // Filter
 const movements = [200, 450, -400, 3000, -650, -130, 70, -1300, 255, 212, -121212];
 
+const deposits = movements.filter((mov, i, arr) => mov > 0);
+// Just to show that we have access, index and the array too.
+console.log(movements);
+console.log(deposits);
 
+// for - of
+const depositFor = [];
+for(const mov of movements) if (mov > 0) depositFor.push(mov);
+console.log(depositFor);
+
+// Example
+
+const withdrawals = movements.filter(mov => mov < 0);
+
+console.log(withdrawals);
+
+// Reduce
