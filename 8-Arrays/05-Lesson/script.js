e6e175cb69ab11b48a4df10059f854c9dc5e7796
jantayreyes <jantayreyes@gmail.com>
2020-11-26 14:40:45 +0100

    Chaining methods

--- 8-Arrays/05-Lesson/script.js
+++ 8-Arrays/05-Lesson/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+
+// PIPELINE
+const movements = [200, 450, -400, 3000, -650, -130, 70, -1300, 255, 212, -1212];
+
+
+// The Magic of Chaining Methods
+const eurToUsd = 1.1;
+
+const totalDepositsUSD = movements
+.filter(mov => mov > 0)
+.map(mov => mov * eurToUsd)
+.reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalDepositsUSD);
