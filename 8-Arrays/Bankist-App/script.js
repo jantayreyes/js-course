87608312467dfc3287f862377c52b154473bfbdf
jantayreyes <jantayreyes@gmail.com>
2020-11-26 15:09:54 +0100

    Calculating Deposits, withdraws, and interests

--- 8-Arrays/Bankist-App/script.js
+++ 8-Arrays/Bankist-App/script.js
@@ -73,7 +73,7 @@ const displayMovements = function(movements) {
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1}${type} </div>
-        <div class="movements__value">${mov}</div>
+        <div class="movements__value">${mov}€</div>
       </div>
     `; 
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -89,6 +89,30 @@ calcDisplayBalance(account1.movements);
 
 // console.log(containerMovements.innerHTML);
 // console.log(containerMovements.textContent);
+
+const calcDisplaySummary = function(movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`
+
+  const out = movements 
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`
+
+  // We have a interest of 1.2% for each deposit (positive value)
+  const interest =  movements
+    .filter(mov => mov > 0)
+    .map(mov => (mov * 1.2) / 100)
+    .filter((int, i, arr) => { // We just add the value, if interest is >= 1
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 const createUsername =  function(accs) {
   accs.forEach(function (acc) {
