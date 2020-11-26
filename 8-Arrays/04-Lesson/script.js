ae4ac8bcdd5e07bd0213a0e9e2ba172561e6a2b9
jantayreyes <jantayreyes@gmail.com>
2020-11-26 12:23:01 +0100

    Reduce

--- 8-Arrays/04-Lesson/script.js
+++ 8-Arrays/04-Lesson/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Filter
-const movements = [200, 450, -400, 3000, -650, -130, 70, -1300, 255, 212, -121212];
+const movements = [200, 450, -400, 3000, -650, -130, 70, -1300, 255, 212, -1212];
 
 const deposits = movements.filter((mov, i, arr) => mov > 0);
 // Just to show that we have access, index and the array too.
@@ -20,3 +20,15 @@ const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
 
 // Reduce
+// In the callback function, the first paramether is the accumulator
+// The reduce method has a second paramater which is the accumulator starts.
+const balance = movements.reduce(function(acc, mov, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + mov
+}, 0);
+console.log(balance);
+
+// for-of
+let balance2 = 0;
+for(const mov of movements) balance2 += mov;
+console.log(balance2);
