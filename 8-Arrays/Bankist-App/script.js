fba4ba9fa7f1f7323bd8fc1287a992fbe131953a
jantayreyes <jantayreyes@gmail.com>
2020-11-27 16:05:03 +0100

    Sort movements

--- 8-Arrays/Bankist-App/script.js
+++ 8-Arrays/Bankist-App/script.js
@@ -62,10 +62,12 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 
-const displayMovements = function(movements) {
+const displayMovements = function(movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
 
     const html = `
@@ -183,7 +185,7 @@ btnTransfer.addEventListener('click', function(e) {
 // REQUEST A BANK LOAN
 btnLoan.addEventListener('click', function(e) {
   e.preventDefault();
-  
+
   const amount = Number(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
@@ -215,4 +217,11 @@ btnClose.addEventListener('click', function(e) {
 
   }
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted
 });
