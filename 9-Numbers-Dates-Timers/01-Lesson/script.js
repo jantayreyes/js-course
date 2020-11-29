b4a3076dd0b1211e82286de1255b87e006b97033
jantayreyes <jantayreyes@gmail.com>
2020-11-29 14:04:07 +0100

    Working with Number methods

--- 9-Numbers-Dates-Timers/01-Lesson/script.js
+++ 9-Numbers-Dates-Timers/01-Lesson/script.js
@@ -7,5 +7,34 @@ console.log(23 === 23.0);
 console.log(0.1 + 0.2);
 console.log(0.1 + 0.2 === 0.3); // False, we have to accept in JS.
 
-console.log(Number('23')); // String to Number
+// String to Number
+console.log(Number('23')); 
 console.log(+'23');
+
+// Parsing
+console.log(Number.parseInt('38px'));
+console.log(Number.parseInt('x38px')); // Not number
+console.log(Number.parseInt('38px', 10)); // second parameter the base.
+
+console.log(Number.parseInt('2.5rem'));
+console.log(Number.parseFloat('2.5rem'));
+
+console.log(parseFloat('2.5rem')); // Old way, now you should call Number first.
+
+// If a value is not a number
+console.log(Number.isNaN(20)); // false
+console.log(Number.isNaN('20')); // false
+console.log(Number.isNaN(+'20x'));  // true
+console.log(Number.isNaN(23 / 0)); // infinity = false
+
+// If a value is a number, a real number - working with decimal
+console.log(Number.isFinite(20)); // true 
+console.log(Number.isFinite('20')); // false
+console.log(Number.isFinite(+'20x'));  // false
+console.log(Number.isFinite(23 / 0)); // infinity = true
+
+// If a value is a number, without decimals.
+console.log(Number.isInteger(23)); // true
+console.log(Number.isInteger(23.0)); // true
+console.log(Number.isInteger(22.5)); // false
+
--- 9-Numbers-Dates-Timers/Bankist-Continue/script.js
+++ 9-Numbers-Dates-Timers/Bankist-Continue/script.js
@@ -1,10 +1,5 @@
 // BANKIST APP
 
-/////////////////////////////////////////////////
-// Data
-
-// DIFFERENT DATA! Contains movement dates, currency and locale
-
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
