a4e33326c21c1b51016bc5721a4d335291180bf2
jantayreyes <jantayreyes@gmail.com>
2020-11-29 13:42:08 +0100

    Numbers

--- 9-Numbers-Dates-Timers/01-Lesson/script.js
+++ 9-Numbers-Dates-Timers/01-Lesson/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+console.log(23 === 23.0);
+
+// Base 10 - 0 to 9 => 1/10 = 0.1, 3/10 = 3.33333333
+// Binary Base 2 - 0 and 1
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3); // False, we have to accept in JS.
+
+console.log(Number('23')); // String to Number
+console.log(+'23');
