70fa89136fca338e264e693b21eb5a8e26d5f5bb
jantayreyes <jantayreyes@gmail.com>
2020-11-30 13:53:57 +0100

    Added Data Times

--- 9-Numbers-Dates-Timers/Bankist-Continue/script.js
+++ 9-Numbers-Dates-Timers/Bankist-Continue/script.js
@@ -177,7 +177,7 @@ btnLogin.addEventListener('click', function(e) {
     const day = `${now.getDate()}`.padStart(2, 0);
     const month = `${now.getMonth()+ 1}`.padStart(2, 0) ;
     const year =  now.getFullYear();
-    const hour = now.getHours();
+    const hour = `${now.getHours()}`.padStart(2, 0);;
     const min = `${now.getMinutes()}`.padStart(2, 0);
     labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`
     
