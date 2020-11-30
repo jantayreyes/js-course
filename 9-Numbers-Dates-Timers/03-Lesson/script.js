46e8714bc98ead3cc55062c4b57b33da365b99d3
jantayreyes <jantayreyes@gmail.com>
2020-11-30 12:03:59 +0100

    Remainder Operator

--- 9-Numbers-Dates-Timers/03-Lesson/script.js
+++ 9-Numbers-Dates-Timers/03-Lesson/script.js
@@ -1,2 +1,25 @@
 'use strict';
 
+// Remainder Operator
+console.log(5 % 2);
+console.log(8 % 3);
+
+console.log(6 % 2);
+console.log(7 % 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(5));
+console.log(isEven(47));
+
+// We have a NodeList, that's why we use spread operator to conver into an array.
+
+labelBalance.addEventListener('click', function() {
+  [...document.querySelectorAll('movements__row')].forEach(function(row, i) {
+    // 0, 2 ,4, 6
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+  })
+  // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+});
+
