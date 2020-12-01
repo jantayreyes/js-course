6e825ea5369afc97bfadb770db8217a2f62f88e6
jantayreyes <jantayreyes@gmail.com>
2020-12-01 18:13:13 +0100

    INTL numbers

--- 9-Numbers-Dates-Timers/04-Lesson/script.js
+++ 9-Numbers-Dates-Timers/04-Lesson/script.js
@@ -10,3 +10,16 @@ const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date( 2037, 3, 24))
 console.log(days1);
 
 //INTERNATIONALIZATION
+const num = 323239293.23;
+
+const options = {
+  style:'currency', //unit, percent
+  unit: 'celsius',
+  currency: 'EUR', // If we set style as currency, we have to set the currency (eur, dollars, pounds, etc).
+  // useGrouping: false - false = without separators.
+};
+
+console.log('US: ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log(navigator.language, new Intl.NumberFormat(navigator.language, options).format(num));
