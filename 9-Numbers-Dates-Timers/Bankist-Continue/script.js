1830d000076f277538827e4a2ae9c49e09bd983e
jantayreyes <jantayreyes@gmail.com>
2020-12-01 16:42:27 +0100

    Formating dates added

--- 9-Numbers-Dates-Timers/Bankist-Continue/script.js
+++ 9-Numbers-Dates-Timers/Bankist-Continue/script.js
@@ -11,10 +11,10 @@ const account1 = {
     '2019-12-23T07:42:02.383Z',
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2020-11-25T14:11:59.604Z',
+    '2020-11-28T17:01:17.194Z',
+    '2020-11-29T23:36:17.929Z',
+    '2020-11-30T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -32,9 +32,9 @@ const account2 = {
     '2019-12-25T06:04:23.907Z',
     '2020-01-25T14:18:46.235Z',
     '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2020-11-27T14:43:26.374Z',
+    '2020-11-25T18:49:59.371Z',
+    '2020-11-30T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -71,20 +71,35 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // +('any number in string format') =  Number('Any number in string format')
+
+const formatMovementsDate = function(date) {
+  const calcDaysPassed = (date1, date2) => 
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  
+  const dayPassed = calcDaysPassed(new Date(), date)
+
+  if (dayPassed === 0) return 'Today';
+  if (dayPassed === 1) return 'Yesterday';
+  if (dayPassed <= 7) return `${dayPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth()+ 1}`.padStart(2, 0) ;
+    const year =  date.getFullYear();
+    return `${day}/${month}/${year}`
+  }
+};
+
 const displayMovements = function(account, sort = false) {
   containerMovements.innerHTML = '';
 
   const movs = sort ? account.movements.slice().sort((a, b) => a - b) : account.movements;
 
   movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal'
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(account.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth()+ 1}`.padStart(2, 0) ;
-    const year =  date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`
-
+    displayDate = formatMovementsDate(date);
+  
     const html = `
       <div class="movements__row">
         <div class="movements__date">${displayDate}</div>
