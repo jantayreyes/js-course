e072765ffc13b706306decdac07951344b9086f4
jantayreyes <jantayreyes@gmail.com>
2020-12-01 16:25:24 +0100

    Operations with dates

--- 9-Numbers-Dates-Timers/04-Lesson/script.js
+++ 9-Numbers-Dates-Timers/04-Lesson/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const future =  new Date(2037, 10, 16, 23, 5);
+
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date( 2037, 3, 24))
+console.log(days1);
