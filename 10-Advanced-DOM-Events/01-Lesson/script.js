c00903fa7eb2966bda215e37fc026fdf79a707ca
jantayreyes <jantayreyes@gmail.com>
2020-12-02 16:27:43 +0100

    Started files

---
+++ 10-Advanced-DOM-Events/01-Lesson/script.js
@@ -0,0 +1,9 @@
+LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
