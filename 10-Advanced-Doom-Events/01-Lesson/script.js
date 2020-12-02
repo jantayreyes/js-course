61c87cdf0b3c9625a95efffabe58e3e2f6f9e5be
jantayreyes <68383799+jantayreyes@users.noreply.github.com>
2020-12-02 16:29:01 +0100

    Delete script.js

--- 10-Advanced-Doom-Events/01-Lesson/script.js
+++
@@ -1,9 +0,0 @@
-LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
