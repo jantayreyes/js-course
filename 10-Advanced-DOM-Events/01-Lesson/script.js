b56ce5086ad52b398a0a8d4b4a6991cd7e95bef0
jantayreyes <jantayreyes@gmail.com>
2020-12-04 16:04:21 +0100

    Handling events and removing them

--- 10-Advanced-DOM-Events/01-Lesson/script.js
+++ 10-Advanced-DOM-Events/01-Lesson/script.js
@@ -85,3 +85,18 @@ logo.classList.contains('c'); // not includes
 
 // DO NOT USE - because it overrides all the existing classes
 logo.className = 'jonas'
+
+// Handling events and removing them
+const h1 =  document.querySelector('h1');
+const alertH1 =  function (e) {
+  alert('addEventListener: Great! You are reading the heading');  
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading');
+// };
+
--- 10-Advanced-DOM-Events/Bankist-Website/script.js
+++ 10-Advanced-DOM-Events/Bankist-Website/script.js
@@ -61,20 +61,6 @@ btnSrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({behavior: 'smooth'})
 });
 
-// Handling events and removing them
-const h1 =  document.querySelector('h1');
-const alertH1 =  function (e) {
-  alert('addEventListener: Great! You are reading the heading');  
-};
-
-h1.addEventListener('mouseenter', alertH1);
-
-setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
-
-// h1.onmouseenter = function (e) {
-//   alert('onmouseenter: Great! You are reading the heading');
-// };
-
 
 
  
