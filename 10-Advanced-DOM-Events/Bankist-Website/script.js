e7990c96e5422b43a0aa97d246d4456b63733413
jantayreyes <jantayreyes@gmail.com>
2020-12-08 13:51:30 +0100

    Refactoring Fade Animation

--- 10-Advanced-DOM-Events/Bankist-Website/script.js
+++ 10-Advanced-DOM-Events/Bankist-Website/script.js
@@ -117,28 +117,54 @@ tabsContainer.addEventListener('click', function(e) {
 });
 
 // Menu fade animation
-nav.addEventListener('mouseover', function(e) {
+const handleHover = function (e) {
+  console.log(this, e.currentTarget);
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
-      if(el !== link) el.style.opacity = 0.5;
+      if(el !== link) el.style.opacity = this;
     });
-    logo.style.opacity = 0.5;
+    logo.style.opacity = this;
   }
-});
+};
 
-nav.addEventListener('mouseout', function(e) {
-  if (e.target.classList.contains('nav__link')) {
-    const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img');
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// });
 
-    siblings.forEach(el => {
-      if(el !== link) el.style.opacity = 1;
-    });
-    logo.style.opacity = 1;
-  }
-});
+// nav.addEventListener('mouseout', function (e) {
+//   handleHover(e, 1)
+// });
+
+// Passing 'argument' into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// nav.addEventListener('mouseover', function(e) {
+//   if (e.target.classList.contains('nav__link')) {
+//     const link = e.target;
+//     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+//     const logo = link.closest('.nav').querySelector('img');
+
+//     siblings.forEach(el => {
+//       if(el !== link) el.style.opacity = 0.5;
+//     });
+//     logo.style.opacity = 0.5;
+//   }
+// });
+
+// nav.addEventListener('mouseout', function(e) {
+//   if (e.target.classList.contains('nav__link')) {
+//     const link = e.target;
+//     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+//     const logo = link.closest('.nav').querySelector('img');
+
+//     siblings.forEach(el => {
+//       if(el !== link) el.style.opacity = 1;
+//     });
+//     logo.style.opacity = 1;
+//   }
+// });
