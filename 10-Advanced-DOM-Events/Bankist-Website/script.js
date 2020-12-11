d0aa029e2774a3b6544f12c09cb0cdcf6760b060
jantayreyes <jantayreyes@gmail.com>
2020-12-11 14:18:19 +0100

    Slider component

--- 10-Advanced-DOM-Events/Bankist-Website/script.js
+++ 10-Advanced-DOM-Events/Bankist-Website/script.js
@@ -231,7 +231,7 @@ const sectionObserver = new IntersectionObserver(reevealSection, {
 
 allSections.forEach(function(section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 // LAZY LOADING IMAGES
@@ -262,3 +262,45 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTarget.forEach(img => imgObserver.observe(img));
+
+// SLIDER COMPONENT
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+
+let curSlide = 0;
+const maxSlide = slides.length
+
+const slider = document.querySelector('.slider');
+slider.style.transform = 'scale(0.3)' // just for better overview
+slider.style.overflow = 'visible'
+
+// slides.forEach((s, i) => s.style.transform = `translateX(${100 * i}%)`)
+// We  multiply 100 * current index and move the slides from the right to the left. This happen because the slides moves 0%, 100%, 200% ...
+
+const goToSlide = function(slide) {
+  slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+};
+
+goToSlide(0);
+
+// Next slide
+const nextSlide = function() {
+  if (curSlide === maxSlide - 1) curSlide = 0;
+  else curSlide++;
+  
+  goToSlide(curSlide);
+ 
+};
+
+// Prev slide
+const prevSlide = function() {
+  if (curSlide === 0) curSlide = maxSlide - 1;
+  else curSlide--;
+
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
