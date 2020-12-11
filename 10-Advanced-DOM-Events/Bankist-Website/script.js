38f10cb4fa8db8746b7a6b77f14bdc1d4753331f
jantayreyes <jantayreyes@gmail.com>
2020-12-11 15:11:21 +0100

    Slider component completed

--- 10-Advanced-DOM-Events/Bankist-Website/script.js
+++ 10-Advanced-DOM-Events/Bankist-Website/script.js
@@ -14,6 +14,10 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer =  document.querySelector('.dots');
 ////////////////////////////////////////////////////////////////////////
 
 // MODAL WINDOW
@@ -264,43 +268,91 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTarget.forEach(img => imgObserver.observe(img));
 
 // SLIDER COMPONENT
-const slides = document.querySelectorAll('.slide');
-const btnLeft = document.querySelector('.slider__btn--left');
-const btnRight = document.querySelector('.slider__btn--right');
+const sliderComponent = function () {
+
+  // Funtions
+  let curSlide = 0;
+  const maxSlide = slides.length
+
+  // Just for development -- Better overview
+  // const slider = document.querySelector('.slider');
+  // slider.style.transform = 'scale(0.3)' 
+  // slider.style.overflow = 'visible'
+
+  // slides.forEach((s, i) => s.style.transform = `translateX(${100 * i}%)`)
+  // We  multiply 100 * current index and move the slides from the right to the left. This happen because the slides moves 0%, 100%, 200% ...
+
+  const createDots = function() {
+    slides.forEach(function(_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend', 
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function(slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList
+      .remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`).classList
+      .add('dots__dot--active');
+  };
+
+  const goToSlide = function(slide) {
+    slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  };
 
 
-let curSlide = 0;
-const maxSlide = slides.length
-
-const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.3)' // just for better overview
-slider.style.overflow = 'visible'
-
-// slides.forEach((s, i) => s.style.transform = `translateX(${100 * i}%)`)
-// We  multiply 100 * current index and move the slides from the right to the left. This happen because the slides moves 0%, 100%, 200% ...
-
-const goToSlide = function(slide) {
-  slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
-};
-
-goToSlide(0);
-
-// Next slide
-const nextSlide = function() {
-  if (curSlide === maxSlide - 1) curSlide = 0;
-  else curSlide++;
+  // Next slide
+  const nextSlide = function() {
+    if (curSlide === maxSlide - 1) curSlide = 0;
+    else curSlide++;
+    
+    goToSlide(curSlide);
+    activateDot(curSlide);
   
-  goToSlide(curSlide);
- 
+  };
+
+  // Prev slide
+  const prevSlide = function() {
+    if (curSlide === 0) curSlide = maxSlide - 1;
+    else curSlide--;
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function() {
+    goToSlide(0);
+    createDots();
+    activateDot(0);
+    
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  // Keyboard event 
+  document.addEventListener('keydown', function(e) {
+    if(e.key === 'ArrowLeft') prevSlide();
+    if(e.key === 'ArrowRight') nextSlide();
+    // e.key === 'ArrowRight' && nextSlide(); Short Circuiting
+  });
+
+  dotContainer.addEventListener('click', function(e) {
+    if(e.target.classList.contains('dots__dot')) {
+      // const slide = e.target.dataset.slide;
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    };
+  });
 };
 
-// Prev slide
-const prevSlide = function() {
-  if (curSlide === 0) curSlide = maxSlide - 1;
-  else curSlide--;
-
-  goToSlide(curSlide);
-};
-
-btnRight.addEventListener('click', nextSlide);
-btnLeft.addEventListener('click', prevSlide);
+sliderComponent();
