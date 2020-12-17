e19be95dd4ff46ecdbae5824118f323925c3797f
jantayreyes <jantayreyes@gmail.com>
2020-12-17 14:01:12 +0100

    ES6 Clases

--- 11-OOP-With-JavaScript/03-Lesson/script.js
+++ 11-OOP-With-JavaScript/03-Lesson/script.js
@@ -1,2 +1,38 @@
 'use strict';
 
+//ES6 Clases
+// Class expression
+// const PersonCL  = class {}
+
+// Class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Mehtods will be added to .prototype property
+  calcAge() {
+    console.log(2020 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`); 
+  }
+
+};
+
+const jessica = new PersonCl('Jessica', 1999)
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function() {
+//   console.log(`Hey ${this.firstName}`);  
+// };
+jessica.greet();
+
+// 1. Clases are NOT hoisted
+// 2. Class are first-class citizes
+// 3. Classes are executed in strict mode
