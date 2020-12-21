e5e4b2817cf1e963ca49ebb717fd6436fd38bd04
jantayreyes <jantayreyes@gmail.com>
2020-12-21 12:47:22 +0100

    Object.create

--- 11-OOP-With-JavaScript/05-Lesson/script.js
+++ 11-OOP-With-JavaScript/05-Lesson/script.js
@@ -1,2 +1,27 @@
 'use strict';
 
+const PersonProto = {
+  calcAge() {
+    console.log(2020 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear =  birthYear;
+  },
+
+};
+
+const steven = Object.create(PersonProto)
+steven.name = 'Steven';
+console.log(steven);
+
+steven.birthYear = 2000;
+steven.calcAge();
+
+console.log(steven.__proto__);
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 2003);
+sarah.calcAge();
